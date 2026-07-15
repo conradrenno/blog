11a421522dbe911c9d28e94c0e4de27839fd2792
conradrenno <conradrenno@hotmail.com>
2026-07-15 16:27:54 -0300

    refactor: use post summary component in post list and featured

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,8 +1,12 @@
 import clsx from "clsx";
 import { PostCoverImage } from "../PostCoverImage";
 import { PostHeading } from "../PostHeading";
+import { PostSummary } from "../PostSummary";
 
 export function PostFeatured() {
+  const slug = "titulo-do-post";
+  const postLink = `/post/${slug}`;
+
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
       <PostCoverImage
@@ -10,21 +14,15 @@ export function PostFeatured() {
         src="/images/bryen_0.png"
         altText="Titulo do post"
       />
-      <div className={clsx("flex flex-col gap-4 sm:justify-center")}>
-        <time className="text-slate-600 text-sm/tight" dateTime="2026-07-03">
-          03/07/2026 11:00
-        </time>
-        <PostHeading url="#" as="h1">
-          Titulo do post
-        </PostHeading>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nunc ut Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          euismod, nunc ut Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Sed euismod, nunc ut Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Sed euismod, nunc ut
-        </p>
-      </div>
+      <PostSummary
+        postHeading="h1"
+        postLink={postLink}
+        createdAt={"2025-04-08T00:33:56.907Z"}
+        title={"Rotina matinal de pessoas altamente eficazes"}
+        excerpt={
+          "O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO."
+        }
+      />
     </section>
   );
 }
--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,7 +1,9 @@
 import { postRepository } from "@/repositories/post";
 import { PostCoverImage } from "../PostCoverImage";
+import { PostSummary } from "../PostSummary";
 import { PostHeading } from "../PostHeading";
 import clsx from "clsx";
+import { formatDateTime, formatDistanceToNow } from "@/utils/format-datetime";
 
 export async function PostList() {
   const posts = await postRepository.findAll();
@@ -18,18 +20,14 @@ export async function PostList() {
               src={post.coverImageUrl}
               altText={post.title}
             />
-            <div className={clsx("flex flex-col gap-4 sm:justify-center")}>
-              <time
-                className="text-slate-600 text-sm/tight"
-                dateTime={post.createdAt}
-              >
-                {post.createdAt}
-              </time>
-              <PostHeading url={postLink} as="h2">
-                {post.title}
-              </PostHeading>
-              <p>{post.excerpt}</p>
-            </div>
+
+            <PostSummary
+              postHeading="h2"
+              postLink={postLink}
+              createdAt={post.createdAt}
+              title={post.title}
+              excerpt={post.excerpt}
+            />
           </div>
         );
       })}
