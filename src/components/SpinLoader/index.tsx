28307441e128dfac354115e209f1c180a19557d3
conradrenno <conradrenno@hotmail.com>
2026-07-18 14:57:54 -0300

    fix: correct SpinLoader border styling

--- src/components/SpinLoader/index.tsx
+++ src/components/SpinLoader/index.tsx
@@ -11,9 +11,8 @@ export function SpinLoader({ className = "" }: SpinLoaderProps) {
     <div className={classes}>
       <div
         className={clsx(
-          "animate-spin rounded-full border-5  border-t-transparent border-slate-900",
+          "animate-spin rounded-full border-4 border-t-transparent border-slate-900",
           "w-10 h-10",
-          classes,
         )}
       ></div>
     </div>
