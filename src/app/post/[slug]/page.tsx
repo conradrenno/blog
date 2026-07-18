71f3ddacbf679e21a9a25086627147b4c84a620e
conradrenno <conradrenno@hotmail.com>
2026-07-18 14:57:31 -0300

    feat: add single post page

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import { SinglePost } from "@/components/SinglePost";
+import { SpinLoader } from "@/components/SpinLoader";
+import { findBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+type PostSlugPageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findBySlugCached(slug);
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
+
+export default async function PostSlugPage({ params }: PostSlugPageProps) {
+  const { slug } = await params;
+
+  return (
+    <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
+      <SinglePost slug={slug} />
+    </Suspense>
+  );
+}
